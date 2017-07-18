f79d22026d11431ecfd6a0b9576d550b7e5b400b
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:24:39 -0400

    Update orcs.h

--- orcs.h
+++ orcs.h
@@ -1,4 +1,13 @@
 #ifndef _ORCS_H_
 #define _ORCS_H_
 
+class Orcs {
+  public:
+  virtual void beAttack(Shade *s);
+  virtual void beAttack(Drow *d);
+  virtual void beAttack(Vampire *v);
+  virtual void beAttack(Troll *t);
+  virtual void beAttack(Goblin *g);
+};
+
 #endif
