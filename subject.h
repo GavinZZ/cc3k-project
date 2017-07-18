7bf4918ff6d1ada19b0487150f18b97932e2931b
Mr. GZ <hz351086153@gmail.com>
2017-07-17 20:59:52 -0400

    Update subject.h

--- subject.h
+++ subject.h
@@ -1,2 +1,13 @@
-#ifndef SUBJECT_H
-#define SUBJECT_H
+#ifndef _SUBJECT_H_
+#define _SUBJECT_H_
+#include <vector>
+class Observer;
+
+class Subject {
+public:
+    void attach(Observer *o);
+    void notifyObservers();
+    virtual ~Subject() = 0;
+};
+
+#endif
