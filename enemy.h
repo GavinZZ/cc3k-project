0ee04e9549fea2cb29c7d7d8f16e98f8d6e440f6
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:22:13 -0400

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -1,4 +1,22 @@
 #ifndef _ENEMY_H_
 #define _ENEMY_H_
+#include "human.h"
+#include "dwarf.h"
+#include "elf.h"
+#include "orcs.h"
+#include "merchant.h"
+#include "dragon.h"
+
+class Enemy {
+  public:
+  void notify(Subject &sub, string action) override;
+  void attack(Player *p);
+  virtual void beAttack(Shade *s);
+  virtual void beAttack(Drow *d);
+  virtual void beAttack(Vampire *v);
+  virtual void beAttack(Troll *t);
+  virtual void beAttack(Goblin *g);
+  void notifyObserver();
+};
 
 #endif
