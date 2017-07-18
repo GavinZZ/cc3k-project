52a9f17a0581ee1dc072e310e7a96b2d25d36fa8
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-17 23:07:58 -0400

    Update observer.h

--- observer.h
+++ observer.h
@@ -2,12 +2,13 @@
 #define _OBSERVER_H_
 #include "subscriptions.h"
 #include "display.h"
+#include <string>
 class Subject;
 class Enemy;
 
 class Observer {
 public:
-    virtual void notify(Subject &whoNotified, string action) = 0;
+    virtual void notify(Subject &whoNotified, std::string action) = 0;
     virtual ~Observer() = default;
 };
 #endif
