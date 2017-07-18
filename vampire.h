79c6938f0f0df59044efc3b7b99f8a9f92ab7f7d
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:10:09 -0400

    Update vampire.h

--- vampire.h
+++ vampire.h
@@ -1,4 +1,14 @@
 #ifndef _VAMPIRE_H_
 #define _VAMPIRE_H_
 
+class Shade {
+  public:
+  void beAttack(Human *h);
+  void beAttack(Dwarf *d);
+  void beAttack(Elf *e);
+  void beAttack(Orcs *o);
+  void beAttack(Merchant *m);
+  void beAttack(Dragon *d);
+};
+
 #endif
