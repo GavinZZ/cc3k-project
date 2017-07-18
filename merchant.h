411613cdb9a8f2c7fa5e1901dae3a2baab61c20a
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:25:23 -0400

    Update merchant.h

--- merchant.h
+++ merchant.h
@@ -1,4 +1,13 @@
 #ifndef _MERCHANT_H_
 #define _MERCHANT_H_
 
+class Merchant {
+  public:
+  virtual void beAttack(Shade *s);
+  virtual void beAttack(Drow *d);
+  virtual void beAttack(Vampire *v);
+  virtual void beAttack(Troll *t);
+  virtual void beAttack(Goblin *g);
+};
+
 #endif
