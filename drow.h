a54c31642d6af485788f4c00c8e8efb69d9c5962
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-17 22:33:16 -0400

    Update drow.h

--- drow.h
+++ drow.h
@@ -1,7 +1,15 @@
 #ifndef _DROW_H_
 #define _DROW_H_
 
-class Shade {
+#include "player.h"
+#include "enemy.h"
+#include "human.h"
+#include "elf.h"
+#include "orcs.h"
+#include "merchant.h"
+#include "dragon.h"
+
+class Drow: public Player{
   public:
   void beAttack(Human *h);
   void beAttack(Dwarf *d);
