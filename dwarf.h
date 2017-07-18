5bc88497711b5c9513e116ab6b75de6b29c20439
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:23:15 -0400

    Update dwarf.h

--- dwarf.h
+++ dwarf.h
@@ -1,4 +1,12 @@
 #ifndef _DWARF_H_
 #define _DWARF_H_
 
+class Dwarf {
+  virtual void beAttack(Shade *s);
+  virtual void beAttack(Drow *d);
+  virtual void beAttack(Vampire *v);
+  virtual void beAttack(Troll *t);
+  virtual void beAttack(Goblin *g);
+};
+
 #endif
