ad506b908ba4f98dfa39e1a86d72ce95efa244fe
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:01:09 -0400

    Update observer.h

--- observer.h
+++ observer.h
@@ -1,4 +1,12 @@
 #ifndef _OBSERVER_H_
 #define _OBSERVER_H_
+#include "subscriptions.h"
+class Subject;
+class Enemy;
 
+class Observer {
+public:
+    virtual void notify(Subject &whoNotified, string action) = 0;
+    virtual ~Observer() = default;
+};
 #endif
