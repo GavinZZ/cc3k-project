947dfe70987955bbf259d162b895b60e5854f735
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:03:34 -0400

    Update character.h

--- character.h
+++ character.h
@@ -1,4 +1,24 @@
 #ifndef _CHARACTER_H_
 #define _CHARACTER_H_
 
+class Character {
+  protected:
+  int health;
+  int attack;
+  int defence;
+  char sign;
+  int col;
+  int row;
+  public:
+  int getHealth();
+  int getAttack();
+  int getDefence();
+  void change(int health = 0, int attack = 0, int defence = 0);
+  Bool isDead();
+  int getCol();
+  int getRow();
+  char getSign();
+  ~Character();
+}
+
 #endif
