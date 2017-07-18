8951a12eb43180af48ad623e8a54130fe3924832
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:09:56 -0400

    Update drow.h

--- drow.h
+++ drow.h
@@ -1,4 +1,14 @@
 #ifndef _DROW_H_
 #define _DROW_H_
 
+class Shade {
+  public:
+  void beAttack(Human *h);
+  void beAttack(Dwarf *d);
+  void beAttack(Elf *e);
+  void beAttack(Orcs *o);
+  void beAttack(Merchant *m);
+  void beAttack(Dragon *d);
+};
+
 #endif
