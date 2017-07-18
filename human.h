05a0d0e20ec3833a052129f25f6c5261cc1061dd
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:22:46 -0400

    Update human.h

--- human.h
+++ human.h
@@ -1,4 +1,12 @@
 #ifndef _HUMAN_H_
 #define _HUMAN_H_
 
+class Human {
+  virtual void beAttack(Shade *s);
+  virtual void beAttack(Drow *d);
+  virtual void beAttack(Vampire *v);
+  virtual void beAttack(Troll *t);
+  virtual void beAttack(Goblin *g);
+};
+
 #endif
