f998f4393edf22cda2c5ef7b80e58c5222072ff0
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-17 22:45:10 -0400

    Update elf.h

--- elf.h
+++ elf.h
@@ -1,7 +1,14 @@
 #ifndef _ELF_H_
 #define _ELF_H_
 
-class Elf {
+#include "shade.h"
+#include "drow.h"
+#include "vanpire.h"
+#include "troll.h"
+#include "goblin.h"
+#include "enemy.h"
+
+class Elf : public Enemy {
   public:
   virtual void beAttack(Shade *s);
   virtual void beAttack(Drow *d);
