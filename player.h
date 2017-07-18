36fcc521419f212b62415f14f571e1a1aa3ff435
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:07:46 -0400

    Update player.h

--- player.h
+++ player.h
@@ -1,4 +1,27 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
+#include "shade.h"
+#include "drow.h"
+#include "troll.h"
+#include "goblin.h"
+#include "vampire.h"
+
+class Player {
+  protected:
+  int gold;
+  public:
+  void notify(Subject &sub, string action) override;
+  virtual void attack(Enemy *enemy);
+  virtual void beAttack(Human *h);
+  virtual void beAttack(Dwarf *d);
+  virtual void beAttack(Elf *e);
+  virtual void beAttack(Orcs *o);
+  virtual void beAttack(Dragon *d);
+  virtual void beAttack(Merchant *m);
+  void changeGold(int gold);
+  int getGold();
+  void notifyObservers();
+};
+
 #endif
