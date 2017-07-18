73e6e5d0409b45b6f518392516ab2b46b5480f66
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-17 22:34:14 -0400

    Update troll.h

--- troll.h
+++ troll.h
@@ -1,7 +1,16 @@
 #ifndef _TROLL_H_
 #define _TROLL_H_
 
-class Shade {
+#include "player.h"
+#include "enemy.h"
+#include "human.h"
+#include "dwarf.h"
+#include "elf.h"
+#include "orcs.h"
+#include "merchant.h"
+#include "dragon.h"
+
+class Troll : public Player {
   public:
   void beAttack(Human *h);
   void beAttack(Dwarf *d);
