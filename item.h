ac839868d35e688023f652e73b1d4d54ce7f4643
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:26:56 -0400

    Update item.h

--- item.h
+++ item.h
@@ -1,4 +1,21 @@
 #ifndef _ITEM_H_
 #define _ITEM_H_
 
+#include "dragontreasure.h"
+#include "treasure.h"
+#include "potion.h"
+#include "stair.h"
+
+class Item {
+  protected:
+  int row;
+  int col;
+  int sign;
+  public:
+  int getCol();
+  int getRow();
+  int getSign();  
+  ~Item();
+};
+
 #endif
