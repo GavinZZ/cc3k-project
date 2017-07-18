a7eb87f3dbead2e425b36dac1cb22af17da91bf8
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:25:03 -0400

    Update dragon.h

--- dragon.h
+++ dragon.h
@@ -1,4 +1,13 @@
 #ifndef _DRAGON_H_
 #define _DRAGON_H_
 
+class Dragon {
+  public:
+  virtual void beAttack(Shade *s);
+  virtual void beAttack(Drow *d);
+  virtual void beAttack(Vampire *v);
+  virtual void beAttack(Troll *t);
+  virtual void beAttack(Goblin *g);
+};
+
 #endif
