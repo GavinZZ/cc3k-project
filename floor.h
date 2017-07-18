b270c8ff26ee63898866ba2d64c3854e43a7b319
Mr. GZ <hz351086153@gmail.com>
2017-07-17 20:58:25 -0400

    Update floor.h

--- floor.h
+++ floor.h
@@ -1,4 +1,17 @@
 #ifndef _FLOOR_H_
 #define _FLOOR_H_
 
+class Floor {
+  public:
+  Floor();
+  Bool isWon();
+  Bool isLost();
+  void move_player(string direction);
+  void use_potion(string direction);
+  void move_enemy();
+  void attack(string direction);
+  void upstair();
+  ~Floor();
+}
+
 #endif
