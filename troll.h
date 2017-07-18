773623d37a8a282fef503778805fe7f68f40b2ae
Mr. GZ <hz351086153@gmail.com>
2017-07-17 21:10:23 -0400

    Update troll.h

--- troll.h
+++ troll.h
@@ -1,4 +1,14 @@
 #ifndef _TROLL_H_
 #define _TROLL_H_
 
+class Shade {
+  public:
+  void beAttack(Human *h);
+  void beAttack(Dwarf *d);
+  void beAttack(Elf *e);
+  void beAttack(Orcs *o);
+  void beAttack(Merchant *m);
+  void beAttack(Dragon *d);
+};
+
 #endif
