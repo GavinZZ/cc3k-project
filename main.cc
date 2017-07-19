1ff631be95c508632c4d9f6f7ecf98d33cce97dc
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-19 12:56:31 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -8,6 +8,7 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     int n = 1;
     Floor *f = nullptr;
+    Player * p = new Shade;// default setting
     if(argc>=2){
         // we have command line arguments
         string filename = argv[1];
@@ -15,7 +16,57 @@ int main(int argc, const char * argv[]) {
         ......;// create a new floor and use f to point to it
     } else if(argc==1){
         // we don't have command line arguments
-        f = new Floor;
+        f = new Floor{p,n};
     }
-    
+    char r;
+    cout<<"select your race: v for vampire, o for orc, d for dwarf, e for elf, and else for shade";
+    cin>>r;
+    if(r == 'v'){
+        Player *temp = p;
+        p = nullptr;
+        delete temp;
+        p = new Vampire;
+    } else if (r =='o'){
+        Player *temp = p;
+        p = nullptr;
+        delete temp;
+        p = new Orc;
+    } else if(r == 'd'){
+        Player *temp = p;
+        p = nullptr;
+        delete temp;
+        p = new Dwarf;
+    } else if(r == 'e'){
+        Player *temp = p;
+        p = nullptr;
+        delete temp;
+        p = new Elf;
+    }
+    while(!f->isWon()){
+        string s;
+        while(cin>>s){
+            if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+                f->mover_player(s);
+            } else if(s == "a"){
+                cin>>s;
+                if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+                  f->attack(s);
+                } else {
+                  cout<<"Wrong direction. Please enter new command again"
+                }
+            } else if(s == "u"){
+                cin>>s;
+                if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+                  f->use_potion(s);
+                } else {
+                  cout<<"Wrong direction. Please enter new command again"
+                }
+            } else if(s=="r"){
+                // reset race? I don't know what to do here
+            } else if(s=="q"){
+                delete f;
+                exit(0);
+            }
+        }
+    }
 }
