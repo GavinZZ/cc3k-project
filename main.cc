f3789cf3f0c41c96ecd6a2ffe86d46e86268b267
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-19 12:33:11 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "floor.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-
-    return 0;
+    int n = 1;
+    Floor *f = nullptr;
+    if(argc>=2){
+        // we have command line arguments
+        string filename = argv[1];
+        ifstream if{filename};
+        ......;// create a new floor and use f to point to it
+    } else if(argc==1){
+        // we don't have command line arguments
+        f = new Floor;
+    }
+    
 }
