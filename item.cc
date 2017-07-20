42dfbe80060b4a292e1c7f9a48f2dfdac5a434a2
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-19 23:18:18 -0400

    Create item.cc

---
+++ item.cc
@@ -0,0 +1,16 @@
+#include "item.h"
+using namespace std;
+
+int Item::getCol(){
+    return col;
+}
+
+int Item::getRow(){
+    return row;
+}
+
+int Item::getSign(){
+    return sign;
+}
+
+Item::~Item(){}
