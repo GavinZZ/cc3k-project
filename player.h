4bfd63d63e1f0c58691de0549167d8162ff7ac20
Mr. GZ <hz351086153@gmail.com>
2017-07-19 20:57:08 -0400

    Update player.h

--- player.h
+++ player.h
@@ -1,31 +1,33 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
 
-
-#include "shade.h"
-#include "drow.h"
-#include "troll.h"
-#include "goblin.h"
-#include "vampire.h"
+#include "subject.h"
+#include "character.h"
 class Enemy;
+class Human;
+class Dwarf;
+class Elf;
+class Orcs;
+class Merchant;
+class Dragon;
 
-class Player ：public Character, public Subject {
-  protected:
-  int gold;
-  int correction_atk;
-  int correction_def;
-  public:
-  void notify(Subject &sub, string action) override;
-  virtual void attack(Enemy *enemy);
-  virtual void beAttack(Human *h);
-  virtual void beAttack(Dwarf *d);
-  virtual void beAttack(Elf *e);
-  virtual void beAttack(Orcs *o);
-  virtual void beAttack(Dragon *d);
-  virtual void beAttack(Merchant *m);
-  void changeGold(int gold);
-  int getGold();
-  void notifyObservers();
+class Player : public Character, public Subject {
+protected:
+    int gold;
+    int correction_atk;
+    int correction_def;
+public:
+    void notify(Subject &sub, std::string action);
+    virtual void attack(Enemy *enemy);
+    virtual void beAttack(Human *h);
+    virtual void beAttack(Dwarf *d);
+    virtual void beAttack(Elf *e);
+    virtual void beAttack(Orcs *o);
+    virtual void beAttack(Dragon *d);
+    virtual void beAttack(Merchant *m);
+    void changeGold(int gold);
+    int getGold();
+    void notifyObservers();
 };
 
 #endif
