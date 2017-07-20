92fef3b7a09c2ca5f141ad0cd14f6ae74fb1ccf8
Mr. GZ <hz351086153@gmail.com>
2017-07-19 20:55:15 -0400

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -2,22 +2,23 @@
 #define _ENEMY_H_
 #include "shade.h"
 #include "drow.h"
-#include "vanpire.h"
+#include "vampire.h"
 #include "troll.h"
 #include "goblin.h"
 #include "observer.h"
+#include "subject.h"
 class Player;
 
-class Enemy : public Observer, public Subject, public Character {
-  public:
-  void notify(Subject &sub, string action) override;
-  void attack(Player *p);
-  virtual void beAttack(Shade *s);
-  virtual void beAttack(Drow *d);
-  virtual void beAttack(Vampire *v);
-  virtual void beAttack(Troll *t);
-  virtual void beAttack(Goblin *g);
-  void notifyObserver();
+class Enemy: public Observer, public Subject, public Character {
+public:
+    void notify(Subject &sub, std::string action) override;
+    virtual void attack(Player *p);
+    virtual void beAttack(Shade *s);
+    virtual void beAttack(Drow *d);
+    virtual void beAttack(Vampire *v);
+    virtual void beAttack(Troll *t);
+    virtual void beAttack(Goblin *g);
+    void notifyObserver();
 };
 
 #endif
