73bc6d9802d6326545649d72b749cb40d5e51863
Mr. GZ <hz351086153@gmail.com>
2017-07-19 20:56:26 -0400

    Update merchant.h

--- merchant.h
+++ merchant.h
@@ -3,18 +3,18 @@
 
 #include "shade.h"
 #include "drow.h"
-#include "vanpire.h"
+#include "vampire.h"
 #include "troll.h"
 #include "goblin.h"
 #include "enemy.h"
 
 class Merchant : public Enemy{
-  public:
-  virtual void beAttack(Shade *s);
-  virtual void beAttack(Drow *d);
-  virtual void beAttack(Vampire *v);
-  virtual void beAttack(Troll *t);
-  virtual void beAttack(Goblin *g);
+public:
+    virtual void beAttack(Shade *s);
+    virtual void beAttack(Drow *d);
+    virtual void beAttack(Vampire *v);
+    virtual void beAttack(Troll *t);
+    virtual void beAttack(Goblin *g);
 };
 
 #endif
