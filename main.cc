5140d0808d64fdbbd6c08cf6e6879b7351cb22b7
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-19 23:14:07 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -7,52 +7,102 @@
 #include "item.h"
 class Shade;
 class Vampire;
-class Orc;
-class Dwarf;
-class Elf;
+class Goblin;
+class Drow;
+class Troll;
 using namespace std;
 
+struct AllFloors{
+    Floor **floors;
+    int n;
+};
+
 int main(int argc, const char * argv[]) {
-    int n = 1;
-    Floor *f = nullptr;
+    AllFloors af;
     Player * p = new Shade;// default setting
+    for(int i = 1; i<6;++i){
+        Floor *f = new Floor{p,i};
+        af.floors[i-1]= f;
+    }
     if(argc>=2){
         // we have command line arguments
         string filename = argv[1];
         ifstream if{filename};
-        ......;// create a new floor and use f to point to it
+        string line;
+        Floor *f = af.floors[0];
+        Display *d = new Display;
+        for(int n = 0; n < 5;n++){
+           for(int i = 0; i<30 ; i++){
+               getline(if,line);
+               if(if.fail()){
+                   break;
+               }
+               for(int j = 0;j<79;++j){
+                   // inteprete items
+                   if(line[j] == '0'){
+                       Display->changeChar(i,j,"RH");
+                   } else if(line[j] == '1'){
+                       Display->changeChar(i,j,"BA");
+                   } else if(line[j] == '2'){
+                       Display->changeChar(i,j,"BD");
+                   } else if(line[j] == '3'){
+                       Display->changeChar(i,j,"PH");
+                   } else if(line[j] == '4'){
+                       Display->changeChar(i,j,"WA");
+                   } else if(line[j] == '5'){
+                       Display->changeChar(i,j,"WD");
+                   } else if(line[j] == '6'){
+                       Display->changeChar(i,j,"NORMAL");
+                   } else if(line[j] == '7'){
+                       Display->changeChar(i,j,"SMALLH");
+                   } else if(line[j] == '8'){
+                       Display->changeChar(i,j,"MERCHANTH");
+                   } else if(line[j] == '9'){
+                       Display->changeChar(i,j,"DRAGONH");
+                   } else if(line[j] == '@'){
+                       Display->changeChar(i,j,"@");
+                       // need a function to change the position of player
+                   } else if(line[j] == ''){
+                       Display->changeChar(i,j,"BD");
+                   }
+                       
+                       
+                
+        
     } else if(argc==1){
         // we don't have command line arguments
         f = new Floor{p,n};
     }
     char r;
-    cout<<"select your race: v for vampire, o for orc, d for dwarf, e for elf, and else for shade";
+    // s, d, v, g, t: specifies the race the player wishes to be when starting a game.
+    cout<<"select your race: v for vampire, g for goblin, d for drow, t for troll, and s for shade";
     cin>>r;
     if(r == 'v'){
         Player *temp = p;
         p = nullptr;
         delete temp;
         p = new Vampire;
-    } else if (r =='o'){
+    } else if (r =='g'){
         Player *temp = p;
         p = nullptr;
         delete temp;
-        p = new Orc;
+        p = new Goblin;
     } else if(r == 'd'){
         Player *temp = p;
         p = nullptr;
         delete temp;
-        p = new Dwarf;
-    } else if(r == 'e'){
+        p = new Drow;
+    } else if(r == 't'){
         Player *temp = p;
         p = nullptr;
         delete temp;
-        p = new Elf;
+        p = new Troll;
     }
     while(!f->isWon()){
         string s;
         while(cin>>s){
             if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+                
                 f->mover_player(s);
             } else if(s == "a"){
                 cin>>s;
@@ -69,7 +119,36 @@ int main(int argc, const char * argv[]) {
                   cout<<"Wrong direction. Please enter new command again"
                 }
             } else if(s=="r"){
-                // reset race? I don't know what to do here
+                delete p;
+                p = new Shade;
+                for(int i = 1; i<6;++i){
+                    Floor *f = new Floor{p,i};
+                    delete af.floors[i-1];
+                    af.floors[i-1]= f;
+                }
+                cout<<"select your race: v for vampire, g for goblin, d for drow, t for troll, and s for shade";
+                cin>>r;
+                if(r == 'v'){
+                    Player *temp = p;
+                    p = nullptr;
+                    delete temp;
+                    p = new Vampire;
+                } else if (r =='g'){
+                    Player *temp = p;
+                    p = nullptr;
+                    delete temp;
+                    p = new Goblin;
+                } else if(r == 'd'){
+                    Player *temp = p;
+                    p = nullptr;
+                    delete temp;
+                    p = new Drow;
+                } else if(r == 't'){
+                    Player *temp = p;
+                    p = nullptr;
+                    delete temp;
+                    p = new Troll;
+                }
             } else if(s=="q"){
                 delete f;
                 exit(0);
