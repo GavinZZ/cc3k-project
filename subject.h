5ce1af9d1ec34e4d5283e19ed2036287de183ecd
Mr. GZ <hz351086153@gmail.com>
2017-07-19 20:57:54 -0400

    Update subject.h

--- subject.h
+++ subject.h
@@ -1,17 +1,15 @@
 #ifndef _SUBJECT_H_
 #define _SUBJECT_H_
 #include <vector>
-#include "player.h"
-#include "enemy.h"
 class Observer;
 
 class Subject {
-   protected:
-    vector<Observer *> observers;
-   public:
-       void attach(Observer *o);
-       void notifyObservers();
-       virtual ~Subject() = 0;
+protected:
+    std::vector<Observer *> observers;
+public:
+    void attach(Observer *o);
+    void notifyObservers();
+    virtual ~Subject() = 0;
 };
 
 #endif
