aff582ce3d5ac269f13ea29fefa88b5d6d19c7fa
Mr. GZ <hz351086153@gmail.com>
2017-07-21 22:56:27 -0400

    Update floor.h

--- floor.h
+++ floor.h
@@ -3,25 +3,32 @@
 #include "player.h"
 #include "display.h"
 #include "item.h"
+#include "stair.h"
 #include "enemy.h"
+#include <vector>
 
 class Floor {
-    std::vector<Enemy *> enemies;
-    std::vector<Item *> items;
-    Player * player;
+    std::vector<std::shared_ptr<Enemy>> enemy;
+    std::vector<std::shared_ptr<Item>> item;
+    std::shared_ptr<Stair> stair;
+    std::shared_ptr<Player> player;
+    std::shared_ptr<Display> display;
     int ith_floor;
+    bool onStair;
+    char previous;
+    void itemSpawn(int row, int col);
+    void enemySpawn(int row, int col);
+    void goldSpawn(int row, int col);
 public:
-    Floor(Player *p = nullptr,int ith = 1);
+    Floor(std::string filename, Player *p, int ith);
+    Floor(Player *p, int ith);
     bool isWon();
     bool isLost();
     void move_player(std::string direction);
     void use_potion(std::string direction);
     void move_enemy();
+    bool getState();
     void attack(std::string direction);
-    void upstair();
-    void changeDisplay(Display *d);
-    void push_item(Item *item);
-    void push_enemy(Enemy *enemy);
     ~Floor();
 };
 
