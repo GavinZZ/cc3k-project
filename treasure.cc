0df65fb89aa0f38de3d78ca074dd94831ace9e36
Mr. GZ <hz351086153@gmail.com>
2017-07-22 21:49:48 -0400

    Create treasure.cc

---
+++ treasure.cc
@@ -0,0 +1,21 @@
+//
+//  treasure.cpp
+//  CC3K
+//
+//  Created by Apple on 2017-07-19.
+//  Copyright © 2017 GavinZ. All rights reserved.
+//
+
+#include "treasure.h"
+using namespace std;
+
+Treasure::Treasure(string s, int row, int col) {
+    sign = s;
+    if (s == "normal") {
+        val = 2;
+    } else if (s == "small") {
+        val = 1;
+    }
+    row = row;
+    col = col;
+}
