afdd80b592de0ab7c22da1c332c65829e72dae42
ericpolo <m8ni@edu.uwaterloo.ca>
2017-07-22 23:21:42 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -12,147 +12,163 @@ class Drow;
 class Troll;
 using namespace std;
 
-struct AllFloors{
-    Floor **floors;
-    int n;
-};
-
 int main(int argc, const char * argv[]) {
-    AllFloors af;
-    Player * p = new Shade;// default setting
-    for(int i = 1; i<6;++i){
-        Floor *f = new Floor{p,i};
-        af.floors[i-1]= f;
-    }
-    if(argc>=2){
-        // we have command line arguments
-        string filename = argv[1];
-        ifstream if{filename};
-        string line;
-        Floor *f = af.floors[0];
-        Display *d = new Display;
-        for(int n = 0; n < 5;n++){
-           for(int i = 0; i<30 ; i++){
-               getline(if,line);
-               if(if.fail()){
-                   break;
-               }
-               for(int j = 0;j<79;++j){
-                   // inteprete items
-                   if(line[j] == '0'){
-                       Display->changeChar(i,j,"RH");
-                   } else if(line[j] == '1'){
-                       Display->changeChar(i,j,"BA");
-                   } else if(line[j] == '2'){
-                       Display->changeChar(i,j,"BD");
-                   } else if(line[j] == '3'){
-                       Display->changeChar(i,j,"PH");
-                   } else if(line[j] == '4'){
-                       Display->changeChar(i,j,"WA");
-                   } else if(line[j] == '5'){
-                       Display->changeChar(i,j,"WD");
-                   } else if(line[j] == '6'){
-                       Display->changeChar(i,j,"NORMAL");
-                   } else if(line[j] == '7'){
-                       Display->changeChar(i,j,"SMALLH");
-                   } else if(line[j] == '8'){
-                       Display->changeChar(i,j,"MERCHANTH");
-                   } else if(line[j] == '9'){
-                       Display->changeChar(i,j,"DRAGONH");
-                   } else if(line[j] == '@'){
-                       Display->changeChar(i,j,"@");
-                       // need a function to change the position of player
-                   } else if(line[j] == ''){
-                       Display->changeChar(i,j,"BD");
-                   }
-                       
-                       
-                
-        
-    } else if(argc==1){
-        // we don't have command line arguments
-        f = new Floor{p,n};
-    }
+    Shade s;
+    Player *p = &s;// default setting
+    cout<<"Welcome to the world of CC3K! Please chouse your character race"<<endl;
     char r;
     // s, d, v, g, t: specifies the race the player wishes to be when starting a game.
-    cout<<"select your race: v for vampire, g for goblin, d for drow, t for troll, and s for shade";
+    cout<<"v for vampire, g for goblin, d for drow, t for troll, and s for shade"<<endl;
+    string race = "Shade";
     cin>>r;
     if(r == 'v'){
-        Player *temp = p;
-        p = nullptr;
-        delete temp;
-        p = new Vampire;
+        Vampire v;
+        p = &v;
+        race = "Vampire";
     } else if (r =='g'){
-        Player *temp = p;
-        p = nullptr;
-        delete temp;
-        p = new Goblin;
+        Goblin g;
+        p = &g;
+        race = "Goblin";
     } else if(r == 'd'){
-        Player *temp = p;
-        p = nullptr;
-        delete temp;
-        p = new Drow;
+        Drow d;
+        p = &d;
+        race = "Drow";
     } else if(r == 't'){
-        Player *temp = p;
-        p = nullptr;
-        delete temp;
-        p = new Troll;
+        Troll t;
+        p = &t;
+        race = "Troll";
     }
-    while(!f->isWon()){
-        string s;
-        while(cin>>s){
-            if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
-                
-                f->mover_player(s);
-            } else if(s == "a"){
-                cin>>s;
-                if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
-                  f->attack(s);
-                } else {
-                  cout<<"Wrong direction. Please enter new command again"
-                }
-            } else if(s == "u"){
-                cin>>s;
-                if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
-                  f->use_potion(s);
-                } else {
-                  cout<<"Wrong direction. Please enter new command again"
-                }
-            } else if(s=="r"){
-                delete p;
-                p = new Shade;
-                for(int i = 1; i<6;++i){
-                    Floor *f = new Floor{p,i};
-                    delete af.floors[i-1];
-                    af.floors[i-1]= f;
-                }
-                cout<<"select your race: v for vampire, g for goblin, d for drow, t for troll, and s for shade";
-                cin>>r;
-                if(r == 'v'){
-                    Player *temp = p;
-                    p = nullptr;
-                    delete temp;
-                    p = new Vampire;
-                } else if (r =='g'){
-                    Player *temp = p;
-                    p = nullptr;
-                    delete temp;
-                    p = new Goblin;
-                } else if(r == 'd'){
-                    Player *temp = p;
-                    p = nullptr;
-                    delete temp;
-                    p = new Drow;
-                } else if(r == 't'){
-                    Player *temp = p;
-                    p = nullptr;
-                    delete temp;
-                    p = new Troll;
-                }
-            } else if(s=="q"){
-                delete f;
-                exit(0);
-            }
+    Floor *f = nullptr;
+    if(argc == 2){
+        string filename = argc[1];
+        Floor floor {filename, p,1};
+        f = &floor;
+    } else if(argc==1){
+        // we don't have command line arguments
+        Floor floor {p,1};
+        f = &floor;
+    }
+    string s;
+    string action = "Player character has spawned." // used to print action mode
+    int n = 1;
+    cout<<f<<endl;
+    cout<<"Race:: "<<race<<" Gold:"<<p->getGold();
+    cout<<"                                                  "<<"Floor: "<<n;
+    cout<<"HP: "<<p->getHealth();<<endl;
+    cout<<"Atk: "<<p->getAttack()<<endl;
+    cout<<"Def: "<<p->getDefence()<<endl;
+    cout<<action<<endl;
+    while(true){
+        if(p->getSign()=="T"){
+            p->change(5);
         }
+        action = "";
+        cin>>S
+        if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+            f->move_player(s);
+            string direction;
+            if(s == "no"){
+                direction = "North";
+            } else if(s == "ne"){
+                direction = "NorthEast";
+            } else if(s == "ea"){
+                direction = "East";
+            } else if(s == "se"){
+                direction = "SouthEast";
+            } else if(s == "so"){
+                direction = "South";
+            } else if(s == "sw"){
+                direction = "SouthWest";
+            } else if(s == "we"){
+                direction = "West";
+            } else if(s == "nw"){
+                direction = "NorthWest";
+            }
+            action += "PC moves ";
+            action += direction;
+            if(f->get_state()){
+                n++;
+                if(n == 6){
+                    cout<<"Congratulation! You reach the end of adventure."<<endl;
+                    cout<<"Do you want to play again: r to play again, q to quit"<<endl;
+                } else {
+                    if(argc == 2){
+                        string filename = argc[1];
+                        Floor floor {filename, p,n};
+                        f = &floor;
+                    } else if(argc==1){
+                        // we don't have command line arguments
+                        Floor floor {p,n};
+                        f = &floor;
+                    }
+                    action += " and go upstair";
+                }
+            } else {
+                f->move_enemy();
+                shared_ptr<Display> display = f->getDisplay();
+                bool state = false;
+                for(int i = -1;i<2;i++){
+                    for(int j = -1;j<2;j++){
+                        char c = display->getChar(p->getRow()+i,p->getCol()+j);
+                        if(c=='P'){
+                            action +=" and see an unknown Potion";
+                            state = true;
+                            break;
+                        }
+                    }
+                    if(state) break;
+                }
+            }
+        } else if(s == "a"){
+            cin>>s;
+            if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+                f->attack(s,action);
+            }
+        } else if(s == "u"){
+            cin>>s;
+            if(s == "no" || s == "ne" || s == "ea" || s == "se" || s == "so" || s == "sw" || s == "we" || s == "nw"){
+                f->use_potion(s,action);
+                f->move_enemy();
+            }
+        } else if(s=="r"){
+            Shade shade;
+            p = &shade;
+            cout<<"Resetting game..."<<endl;
+            cout<<"select your race: v for vampire, g for goblin, d for drow, t for troll, and s for shade"<<endl;
+            cin>>r;
+            if(r == 'v'){
+                Vampire v;
+                p = &v;
+            } else if (r =='g'){
+                Goblin g;
+                p = &g;
+            } else if(r == 'd'){
+                Drow d;
+                p = &d;
+            } else if(r == 't'){
+                Troll t;
+                p = &t;
+            }
+            n = 1;
+            if(argc == 2){
+                string filename = argc[1];
+                Floor floor {filename, p,1};
+                f = &floor;
+            } else if(argc==1){
+                // we don't have command line arguments
+                Floor floor {p,1};
+                f = &floor;
+            }
+        } else if(s=="q"){
+            cout<<"Thanks for your adventure. Good luck next time"<<endl;
+            break;
+        }
+        cout<<f;
+        cout<<"Race:: "<<race<<" Gold:"<<p->getGold();
+        cout<<"                                                  "<<"Floor: "<<n;
+        cout<<"HP: "<<p->getHealth();<<endl;
+        cout<<"Atk: "<<p->getAttack()<<endl;
+        cout<<"Def: "<<p->getDefence()<<endl;
+        cout<<action<<endl;
     }
 }
