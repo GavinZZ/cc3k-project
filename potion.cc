a4d371db0ae9e993c1af312e59386d29cfd5d8f1
Mr. GZ <hz351086153@gmail.com>
2017-07-22 21:48:04 -0400

    Create potion.cc

---
+++ potion.cc
@@ -0,0 +1,17 @@
+//
+//  potion.cpp
+//  CC3K
+//
+//  Created by Apple on 2017-07-19.
+//  Copyright © 2017 GavinZ. All rights reserved.
+//
+
+#include "potion.h"
+using namespace std;
+
+Potion::Potion(string s, int row, int col) {
+    val = -1;
+    sign = s;
+    row = row;
+    col = col;
+}
