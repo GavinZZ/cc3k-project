a69ae4e21fbd38ac305758dd1bd5505a78b4df94
Mr. GZ <hz351086153@gmail.com>
2017-07-22 21:43:20 -0400

    Update character.h

--- character.h
+++ character.h
@@ -2,6 +2,7 @@
 #define _CHARACTER_H_
 
 
+
 class Character {
 protected:
     int health;
@@ -10,17 +11,19 @@ protected:
     char sign;
     int col;
     int row;
-    int correction_atk;
-    int correction_def;
+    bool isHostile;
 public:
+    bool getHostile();
     int getHealth();
     int getAttack();
     int getDefence();
     void change(int health = 0, int attack = 0, int defence = 0);
+    void changePos(int row, int col);
     bool isDead();
     int getCol();
     int getRow();
     char getSign();
+    void healthLost(int damage);
     ~Character();
 };
 
