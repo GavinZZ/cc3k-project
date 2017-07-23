b0ec63f3d467814c80d9e4717e095697499136e8
Mr. GZ <hz351086153@gmail.com>
2017-07-22 21:52:02 -0400

    Update treasure.cc

--- treasure.cc
+++ treasure.cc
@@ -19,3 +19,11 @@ Treasure::Treasure(string s, int row, int col) {
     row = row;
     col = col;
 }
+
+int Treasure::getVal() {
+    if (sign == "small") {
+        return 1;
+    } else if (sign == "normal") {
+        return 2;
+    }
+}
